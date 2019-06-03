a3f01f9cd039e3f93d17805235a0dfa9be903375
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-06-03 14:58:42 -0500

    array notes

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -2,6 +2,70 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+// C requires importing system / header files
+// This is when you import functionalities from other libraries. 
+//These could contain functions that you would like to use in  your program so you have to import them. 
+//You typically include these at the top of your C file. 
+//These could be header files from the standard library or header files that you write.
+
+// NOTES: 
+// Arrays:
+
+// What is an array?
+// And array is a sequences of elements of the same type stored in a contiguous block of memory.
+
+// 1. Each element must be the same type. 
+// 2. They must be in a contiguous block of memory.
+
+// How do you declare an array?
+// 1. Determine how big the array needs to be
+// 2. Request a block of memory that will fit the array
+// 3. Receive the memory address of the reserved block
+// 4. Write your values into the array
+
+// Declare the array: [2, 3, 4, 5]
+// 1. Determine how big the array needs to be
+// 	- An integer is 4 bytes, so the array needs to be 16 bytes
+// 2. Request a block of memory that will fit the array
+// 	- Request 16 bytes of memory from the computer
+// 3.  Receive the memory address of the reserved block 
+// 	- Receive an address to the start of 16 bytes of reserved memory
+// 4. Write your values into the array
+// 	- Write the values of 2, 3, 4, 5 into the array 
+
+// 10 in binary is 2
+// 11 is 3
+// 100 is 4
+// 101 is 5
+
+// Arrays are the most time and space efficient data structures that we have.
+
+// What is a Python List?
+// Python lists are arrays with a lot of built-in functionality. Basically arrays.
+
+// How do you add an element to the end of an array?
+// 1. Take the size of your current array and increase it by one element.
+// 2. Request a block of memory of the new size. 
+// 3. Copy each element from the old space to the new space one at a time.
+// 4. Free the memory from the old array.
+// 	- This is an O(n) operation! 
+
+// How does Python add an element to the end of a list?
+// 	- Python will allocate a few empty spaces each time the array grows.
+// 	- Each time it grows, it allocates a bit more extra space than the previous
+// 	- Adding an element to the end of a list is usually O(1) but sometimes O(n)
+// 	- On average, can be considered O(1)
+
+// How does Python add an element to the beginning of a list? 
+// 1. Check is there's any empty space at the end of the array
+// 2. If not:
+// 	- Allocate a new array, place the first element and copy over the rest
+// 	- Then free memory from the old array.
+// 3. If so: 
+// 	- Starting from the back, move EACH element tot he right one space
+// 	- Place the new element in the first position
+// - This is an 0(n) operation no matter what!
+
 
 typedef struct Array {
   int capacity;  // How many elements can this array hold?
